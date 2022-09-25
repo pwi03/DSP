ca3b4e35df079059730a547923869566f7dd98d5
pwi03 <77678705+pwi03@users.noreply.github.com>
2022-09-25 20:58:28 +0200

    Update main.py

--- main.py
+++ main.py
@@ -21,178 +21,80 @@ blutung_abdruecken = "audios\\blutung_abdruecken.wav"
 lehrkraft_warten = "audios\\lehrkraft_warten.wav"
 lied_hlw = "audios\\lied-hlw.wav"
 #
-MyText = ""
-#functions for questions + textanalysis
+MyText = "START"
+
 def playAudio(MyAudio):
     wave_obj = sa.WaveObject.from_wave_file(MyAudio) #take audio
     play_obj = wave_obj.play() #play audio
-    play_obj.wait_done() #wait for end
+    play_obj.wait_done()
+
 def checkStop(MyText):
-    if MyText == "stopp":
-        print("Programm durch 'Stopp' beendet.")
-        exit(5)
+    if MyText.lower() == "stop":
+        exit(100)
+
+def getAnswer():
+    MyText = "START"
+    with sr.Microphone() as source2:  # micro as input
+        r.adjust_for_ambient_noise(source2, duration=0.2)  # adjust to backround noises
+        audio2 = r.listen(source=source2, phrase_time_limit=3)  # listen to input(user talk)
+    try:
+        MyText = r.recognize_google(audio2, language="de-DE")  # using google to recognize, german as language
+        MyText = MyText.lower()
+        print(MyText)
+        checkStop(MyText)
+    except Exception as e:  # errorhandling
+        print(e)
+    wdh = True
+    while wdh:
+        if MyText == "":
+            MyText = "ERROR"
+
+        if MyText == "ja":
+            return "ja"
+        elif MyText == "nein":
+            return "nein"
+        else:
+            print("wdhx")
+            playAudio(wiederholen)
+            with sr.Microphone() as source2:  # micro as input
+                r.adjust_for_ambient_noise(source2, duration=0.2)  # adjust to backround noises
+                audio2 = r.listen(source=source2, phrase_time_limit=3)  # listen to input(user talk)
+            try:
+                MyText = r.recognize_google(audio2, language="de-DE")  # using google to recognize, german as language
+                MyText = MyText.lower()
+                print(MyText)
+                checkStop(MyText)
+            except Exception as e:
+                print(e)
+    
 #EINLEITUNGSAUDIO
 playAudio(einleitung)
 ##########
 #IST EINE LEHRKRAFT ANWESEND?
 ##########
 playAudio(lehrkraft)
-with sr.Microphone () as source2: # micro as input
-    r.adjust_for_ambient_noise(source2, duration=0.2) #adjust to backround noises
-    audio2 = r.listen(source=source2) # listen to input(user talk)
-try:
-    MyText = r.recognize_google(audio2, language = "de-DE") #using google to recognize, german as language
-    MyText = MyText.lower()
-    print(MyText)
-    checkStop(MyText)
-except Exception as e: #errorhandling
-    MyText = "hallo"
-wdh = True
-while wdh:
-    if MyText == "ja":
-        wdh = False
-    elif MyText == "nein":
-        playAudio(sekretariat)
-        wdh = False
-    else:
-        playAudio(wiederholen)
-        with sr.Microphone () as source2: # micro as input
-            r.adjust_for_ambient_noise(source2, duration=0.2) #adjust to backround noises
-            audio2 = r.listen(source=source2) # listen to input(user talk)
-        try:
-            MyText = r.recognize_google(audio2, language = "de-DE") #using google to recognize, german as language
-            MyText = MyText.lower()
-            print(MyText)
-            checkStop(MyText)
-        except Exception as e:
-            MyText = "hallo"
-##########
-#KONTAKT ZU STROM ODER FEUER?
-##########
+
+if getAnswer() == "nein":
+    playAudio(sekretariat)
+
 playAudio(strom_feuer)
-with sr.Microphone () as source2: # micro as input
-    r.adjust_for_ambient_noise(source2, duration=0.2) #adjust to backround noises
-    audio2 = r.listen(source=source2) # listen to input(user talk)
-try:
-    MyText = r.recognize_google(audio2, language = "de-DE") #using google to recognize, german as language
-    MyText = MyText.lower()
-    print(MyText)
-    checkStop(MyText)
-except Exception as e:
-    MyText = "hallo"
-wdh = True
-while wdh:
-    if MyText == "ja":
-        playAudio(notaus)
-        wdh = False
-    elif MyText == "nein":
-        wdh = False
-    else:
-        playAudio(wiederholen)
-        with sr.Microphone () as source2: # micro as input
-            r.adjust_for_ambient_noise(source2, duration=0.2) #adjust to backround noises
-            audio2 = r.listen(source=source2) # listen to input(user talk)
-        try:
-            MyText = r.recognize_google(audio2, language = "de-DE") #using google to recognize, german as language
-            MyText = MyText.lower()
-            print(MyText)
-            checkStop(MyText)
-        except Exception as e:
-            MyText = "hallo"
-##########
-#IST DIE PERSON ANSPRECHBAR?
-##########
+
+if getAnswer() == "ja":
+    playAudio(notaus)
+
 playAudio(ansprechbar)
-with sr.Microphone () as source2: # micro as input
-    r.adjust_for_ambient_noise(source2, duration=0.2) #adjust to backround noises
-    audio2 = r.listen(source=source2) # listen to input(user talk)
-try:
-    MyText = r.recognize_google(audio2, language = "de-DE") #using google to recognize, german as language
-    MyText = MyText.lower()
-    print(MyText)
-    checkStop(MyText)
-except Exception as e:
-    MyText = "hallo"
-answer_ansprechbar = False
-wdh = True
-while wdh:
-    if MyText == "ja":
-        answer_ansprechbar = True
-        playAudio(beruhigen)
-        wdh = False
-    elif MyText == "nein":
-        answer_ansprechbar = False
-        wdh = False
-    else:
-        playAudio(wiederholen)
-        with sr.Microphone () as source2: # micro as input
-            r.adjust_for_ambient_noise(source2, duration=0.2) #adjust to backround noises
-            audio2 = r.listen(source=source2) # listen to input(user talk)
-        try:
-            MyText = r.recognize_google(audio2, language = "de-DE") #using google to recognize, german as language
-            MyText = MyText.lower()
-            print(MyText)
-            checkStop(MyText)
-        except Exception as e:
-            MyText = "hallo"
-if answer_ansprechbar:
+
+if getAnswer() == "ja":
+    playAudio(beruhigen)
     playAudio(blut)
-    with sr.Microphone () as source2: # micro as input
-        r.adjust_for_ambient_noise(source2, duration=0.2) #adjust to backround noises
-        audio2 = r.listen(source=source2) # listen to input(user talk)
-    try:
-        MyText = r.recognize_google(audio2, language = "de-DE") #using google to recognize, german as language
-        MyText = MyText.lower()
-        print(MyText)
-        checkStop(MyText)
-    except Exception as e:
-        MyText = "hallo"
-    wdh = True
-    while wdh:
-        if MyText == "ja":
-            playAudio(blutung_abdruecken)
-            playAudio(lehrkraft_warten)
-            print("Ende")
-            exit(1)
-        elif MyText == "nein":
-            playAudio(lehrkraft_warten)
-            print("Ende")
-            exit(1)
-        else:
-            playAudio(wiederholen)
-            with sr.Microphone () as source2: # micro as input
-                r.adjust_for_ambient_noise(source2, duration=0.2) #adjust to backround noises
-                audio2 = r.listen(source=source2) # listen to input(user talk)
-            try:
-                MyText = r.recognize_google(audio2, language = "de-DE") #using google to recognize, german as language
-                MyText = MyText.lower()
-                print(MyText)
-                checkStop(MyText)
-            except Exception as e:
-                MyText = "hallo"
-else:
+    if getAnswer() == "ja":
+        playAudio(blutung_abdruecken)
+    playAudio(lehrkraft_warten)
+elif getAnswer() == "nein":
     playAudio(atmen)
-    with sr.Microphone () as source2: # micro as input
-        r.adjust_for_ambient_noise(source2, duration=0.2) #adjust to backround noises
-        audio2 = r.listen(source=source2) # listen to input(user talk)
-    try:
-        MyText = r.recognize_google(audio2, language = "de-DE") #using google to recognize, german as language
-        MyText = MyText.lower()
-        print(MyText)
-        checkStop(MyText)
-    except Exception as e:
-        MyText = "hallo"
-    wdh = True
-    while wdh:
-        if MyText == "ja":
-            playAudio(stabile_seitenlage)
+    if getAnswer() == "ja":
+        playAudio(stabile_seitenlage)
+    elif getAnswer() == "nein":
+        while 1 == 1:
             playAudio(lehrkraft_warten)
-            print("Ende")
-            exit(1)
-        elif MyText == "nein":
-            playAudio(lehrkraft_warten)
-            playAudio(wiederbeleben)
-            while 1 == 1:
-                playAudio(lied_hlw)
-print("Ende")
-exit(-1)
+            playAudio(lied_hlw)
